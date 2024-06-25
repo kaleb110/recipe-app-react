3da467c9860473916b1394d37ed65569299f7312
kaleb110 <137324547+kaleb110@users.noreply.github.com>
2024-06-25 22:58:37 +0300

    main commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Alphabet from "./routes/Alphabet";
 import AlphabetLetter from "./routes/AlphabetLetter";
 import Search from "./routes/Search";
 import ErrorPage from "./components/ErrorPage";
-// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { useState, useEffect } from "react";
 
@@ -12,7 +11,7 @@ function App() {
   const [meal, setmeal] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [url, seturl] = useState(
-    "http://www.themealdb.com/api/json/v1/1/search.php?s="
+    "https://www.themealdb.com/api/json/v1/1/search.php?s="
   );
 
   const fetchData = async () => {
@@ -35,7 +34,7 @@ function App() {
   }, [url]);
 
   const handleClick = () => {
-    seturl(`http://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    seturl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
   };
 
   const truncateText = (text, size) => {
@@ -47,16 +46,15 @@ function App() {
     const targetId = e.target.id;
     let newUrl = "";
     if (targetId === "search") {
-      newUrl = `http://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+      newUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
     } else if (targetId === "home") {
-      newUrl = "http://www.themealdb.com/api/json/v1/1/search.php?s=";
+      newUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
     } else if (targetId === "alphabet") {
-      newUrl = "http://www.themealdb.com/api/json/v1/1/search.php?s=";
+      newUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
     } else {
-      newUrl = `http://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`;
+      newUrl = `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`;
     }
     seturl(newUrl);
-    // fetchData();
   };
 
   const router = createBrowserRouter(
@@ -111,81 +109,6 @@ function App() {
   return (
     <div className="flex flex-col gap-3 py-4 px-4 sm:px-12 bg-slate-100 min-h-screen">
       <RouterProvider router={router} />
-
-      {/* {<Router basename="/Recipe-app-react">
-        <header>
-          <nav>
-            <div className="sm:px-8">
-              <ul className="justify-between flex ">
-                <Link
-                  to="/"
-                  onClick={(e) => handleBtnClick(e)}
-                  className="home"
-                >
-                  <li className="text-gray-500 font-bold hover:cursor-pointer hover:text-gray-600 leading-1 max-sm:text-[16px]">
-                    Home
-                  </li>
-                </Link>
-                <div className="flex gap-4">
-                  <Link
-                    to="/search"
-                    onClick={(e) => handleBtnClick(e)}
-                    className="search"
-                  >
-                    <li className="text-gray-500 font-bold hover:cursor-pointer hover:text-gray-600 leading-1 max-sm:text-[16px]">
-                      Search
-                    </li>
-                  </Link>
-                  <Link
-                    to="/alphabet"
-                    onClick={(e) => handleBtnClick(e)}
-                    className="alphabet"
-                  >
-                    <li className="text-gray-500 font-bold hover:cursor-pointer hover:text-gray-600 leading-1 max-sm:text-[16px]">
-                      Alphabet
-                    </li>
-                  </Link>
-                </div>
-              </ul>
-            </div>
-          </nav>
-        </header>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home meal={meal} truncateText={truncateText} />}
-          />
-          <Route
-            path="/search"
-            element={
-              <Search
-                setquery={setquery}
-                handleClick={handleClick}
-                isLoading={isLoading}
-                meal={meal}
-                truncateText={truncateText}
-              />
-            }
-           />
-          <Route
-            path="/alphabet"
-            element={
-              <Alphabet
-                meal={meal}
-                truncateText={truncateText}
-                onClick={handleBtnClick}
-              />
-            }
-          >
-            <Route
-              path=":letter"
-              element={
-                <AlphabetLetter meal={meal} truncateText={truncateText} />
-              }
-            />
-          </Route>
-        </Routes>
-      </Router>} */}
     </div>
   );
 }
